dd8468aa074123a30f1b55c1ee8a6bcc2fd2b83d
Nick Gurr <0702627@gmail.com>
2024-12-08 23:01:39 +0200

    Додано тест для перегляду списку завдань проекту

--- ProjectManagementSystem4.Tests/ProjectManagerTests .cs
+++ ProjectManagementSystem4.Tests/ProjectManagerTests .cs
@@ -50,6 +50,32 @@ namespace ProjectManagementSystem4.Tests
             Assert.Equal("Виконується", updatedTask.Status);
         }
 
+        [Fact]
+        public void Should_Return_All_Tasks_For_Project()
+        {
+            // Arrange
+            var projectManager = new ProjectManager();
+            var project = new Project("Project A", DateTime.Now, DateTime.Now.AddDays(30));
+
+            var teamMember1 = new TeamMember("Nick Gurr", 1);
+            var teamMember2 = new TeamMember("Anna", 2);
+
+            var task1 = new Task("Implement some feature", teamMember1, "Заплановано");
+            var task2 = new Task("Fix bug Y", teamMember2, "Виконується");
+
+            projectManager.AddProject(project);
+            projectManager.AddTask("Project A", task1);
+            projectManager.AddTask("Project A", task2);
+
+            // Act
+            var tasks = projectManager.GetTasks("Project A");
+
+            // Assert
+            Assert.Equal(2, tasks.Count);
+            Assert.Contains(task1, tasks);
+            Assert.Contains(task2, tasks);
+        }
+
     }
 }
 
